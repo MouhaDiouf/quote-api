bc9534dfe3617ca896a6295252923748591edbc0
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2021-03-23 11:49:39 +0000

    Create route to the quote api

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static("public"));
+
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+
+  res.status(200).send(randomQuote);
+});
+
+app.listen(PORT, () => {
+  console.log("listening at port ", PORT);
+});
